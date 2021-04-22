0421f3b4358f848d38f16de0130cceea57e68845
Woolfer0097 <Woolfer0097@yandex.ru>
2021-04-22 20:47:07 +0300

    Final

--- main.py
+++ main.py
@@ -243,4 +243,5 @@ def logout():
 
 if __name__ == '__main__':
     db_session.global_init("db/books.db")
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
